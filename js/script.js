9573f9580762fdd49aabb4263b4307d5783cdeb1
Beniamino-Soccio <beniamino43@gmail.com>
2024-09-03 17:29:20 +0200

    added img on page (bonus) + end of exercise

--- js/script.js
+++ js/script.js
@@ -11,7 +11,9 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        subtitle: 'Evan You creator of Vue.js',
+        imageLink: 'https://images.ctfassets.net/zojzzdop0fzx/4xN2UpeCLW0Skkj9sqlW4t/9a3c344547f631ec77f2f68cd013c3f7/Eng_Leadership_-_Erica_Stanley.png'
       }
     }
   }).mount('#app')
